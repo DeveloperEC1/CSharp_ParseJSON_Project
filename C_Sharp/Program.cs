05f5d58730a0179cf92299b53d1a2940a81328cd
eliorcohen123 <44778837+eliorcohen123@users.noreply.github.com>
2019-10-14 18:46:11 +0300

    Add files via upload

---
+++ C_Sharp/Program.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace TMDBAppJSONNameSpace
+{
+    class TMDBAppJSONProgram
+    {
+        static void Main(string[] args)
+        {
+            TMDBAppParseJSON();
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+
+        public static async void TMDBAppParseJSON()
+        {
+            string API_KEY = "";
+            string baseUrl = "https://api.themoviedb.org/3/search/movie?/&query=Movie&api_key=" + API_KEY + "&language=en-US";
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    using (HttpResponseMessage res = await client.GetAsync(baseUrl))
+                    {
+                        using (HttpContent content = res.Content)
+                        {
+                            var data = await content.ReadAsStringAsync();
+                            if (content != null)
+                            {
+                                for (int i = 0; i <= data.Length; i++)
+                                {
+                                    Console.WriteLine("Title: " + JObject.Parse(data)["results"][i]["title"] + ".\nOverview: " + JObject.Parse(data)["results"][i]["overview"] + ".\nVote Average: " + JObject.Parse(data)["results"][i]["vote_average"] + "\n");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("NO Data----------");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Exception Hit------------");
+                Console.WriteLine(exception);
+            }
+        }
+    }
+}
